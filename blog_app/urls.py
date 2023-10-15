4b86ee60a7d73c675910e38603ae6810bb04cbf5
Vlad <vmusaelyae@gmail.com>
2023-10-15 21:49:01 +0300

    added reset password logic

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -18,11 +18,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
     path('users/', include('users.urls', namespace='users')),
+
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+         name='reset_password'),
+
+    path('password_reset/sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/reset_password_sent_page.html'),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_form.html'),
+         name='password_reset_confirm'),
+
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
