2dc7057847dc60b71a25f75b6d9d4d13e0947f5c
Vlad <vmusaelyae@gmail.com>
2023-10-16 12:00:15 +0300

    added logic of deleting comment by author of post

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import FeedView, like_view, CreatePostView, ShowPostView
 
@@ -5,8 +6,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('feed/', FeedView.as_view(), name='feed'),
-    path('post/<int:post_id>/', FeedView.as_view(), name='feed_page_with_pk'),
-    path('like/<int:post_id>/', like_view, name='like_post'),
+    path('post/<int:post_id>/', login_required(FeedView.as_view()), name='feed_page_with_pk'),
+    path('like/<int:post_id>/', login_required(like_view), name='like_post'),
     path('create_post/', CreatePostView.as_view(), name='create_post'),
     path('post/detail/<int:pk>', ShowPostView.as_view(), name='post'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 
-from blog.models import Post
+from blog.models import Post, Comment
 from users.forms import CommentForm
 
 
 class CommentFormMixin:
     def post(self, request, *args, **kwargs):
+        if request.POST.get('action') == 'delete':
+            return self.delete_comment(request, *args, **kwargs)
+
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
@@ -14,4 +18,12 @@ class CommentFormMixin:
             comment.save()
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         return self.get(request, *args, **kwargs)
+
+    def delete_comment(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=request.POST.get('comment_id'))
+        if comment.post.author == request.user:
+            comment.delete()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        return self.get(request, *args, **kwargs)
+
 
